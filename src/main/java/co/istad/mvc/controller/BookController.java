ba45e271227e1f2f4275bebb5c5aa322c88a3eb3
it.chhaya <it.chhaya@gmail.com>
2024-09-25 18:41:25 +0700

    test annotation API and Template Engine

--- src/main/java/co/istad/mvc/controller/BookController.java
+++ src/main/java/co/istad/mvc/controller/BookController.java
@@ -1,4 +1,44 @@
 package co.istad.mvc.controller;
 
+import co.istad.mvc.domain.Book;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class BookController {
+
+    @ResponseBody
+    @GetMapping("/books")
+    public Book getBook() {
+        Book book = new Book();
+        book.setId(1);
+        book.setCode("B-001");
+        book.setTitle("Avenger");
+        book.setDescription("Description");
+        book.setThumbnail("/img/01.png");
+        book.setStatus(true);
+
+        return book;
+    }
+
+    @GetMapping("/books/view")
+    public String viewBook(Model model) {
+
+        Book book = new Book();
+        book.setId(1);
+        book.setCode("B-001");
+        book.setTitle("Avenger");
+        book.setDescription("Description");
+        book.setThumbnail("/img/01.png");
+        book.setStatus(true);
+
+        model.addAttribute("book", book);
+
+        return "book";
+    }
+
 }
